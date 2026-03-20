d473bc5dfde1434234383fc6d218d2cd11ec723e
plevagen <varkginglouh@gmail.com>
2026-03-20 16:33:14 +0500

    ads

---
+++ src/thirdTask.java
@@ -0,0 +1,24 @@
+class thirdTask {
+    public static boolean primeCheck(int n, int i){
+        if(n < 2){
+            System.out.println("neither");
+            return false;
+        }
+        if(n == 2){
+            System.out.println("prime");
+            return true;
+        }
+        if(n % i == 0){
+            System.out.println("composite");
+            return false;
+        }
+        if(i * i > n){
+            System.out.println("prime");
+            return true;
+        }
+        return primeCheck(n, i + 1);
+    }
+    public static void main(String[] args){
+        System.out.println(primeCheck(11, 2));
+    }
+}
